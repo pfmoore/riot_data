ef9bf6cab721d41f3f4f67f2073e3a81767bbf61
Paul Moore <p.f.moore@gmail.com>
2017-11-02 22:50:49 +0000

    Tidying up for summoner prepull

--- prepull_summoners.py
+++ prepull_summoners.py
@@ -1,6 +1,7 @@
 from cassiopeia.core import Summoner
 from pathlib import Path
 import json
+from tqdm import tqdm
 
 
 def get_data(path):
@@ -10,10 +11,22 @@ def get_data(path):
     return data["summoners"]["seen"], data["summoners"]["processed"]
 
 
+def cached_ids():
+    cache = Path('cache/cass')
+    summoners = set(int(s.name.split('.')[2]) for s in cache.glob('Sum*'))
+    return summoners
+
+
 if __name__ == "__main__":
     import cassiopeia
     cassiopeia.apply_settings("settings.json")
     seen, processed = get_data(Path("progress.json"))
-    for id in seen:
+    cached = cached_ids()
+    to_load = list((set(seen) | set(processed)) - cached)
+    for id in tqdm(to_load, ascii=True):
         summoner = Summoner(id=id)
-        print(summoner.name)
+        try:
+            # Don't worry about exceptions
+            x = summoner.name
+        except Exception:
+            pass
